1b6cfe6f57a98fe80fdfb34fea6cd0266ddcad83
JrainLau <jrainlau@163.com>
2019-05-17 17:40:39 +0800

    fix: remove pwa

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'highlight.js/styles/github.css'
